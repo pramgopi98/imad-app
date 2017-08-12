720efeef4ac61fe2fa7fed7a904b058f67e15e19
pramgopi98 <30866528+pramgopi98@users.noreply.github.com>
2017-08-12 13:49:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,17 +4,6 @@ var path = require('path');
 
 
 
-
-
-
-
-var app = express();
-app.use(morgan('combined'));
-
-app.get('/', function (req, res) {
-  res.send(createtemplate(articleOne));
-});
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
