fe226b30b4489fd6e5f9a844be19d0b014df522c
pramgopi98 <30866528+pramgopi98@users.noreply.github.com>
2017-08-13 11:49:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,24 @@
-console.log('Loaded!');
-
-var element= document.getElementById('main-text');
-element.innerHTML = 'New Value';
-
-var img =document.getElementById('madi');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft='px';
+//counter code
+var button=document.getElementById('button');
+button.onclick=function(){
+    //Make a request to counter endpoint
+    var request = new XMLHttpRequest();
+    //capture response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState===XMLHttprequest.DONE){
+            //Take some action
+            if(request.status===200){
+                var counter=request.responseText;
+                counter=counter+1;
+                var span= document.getElementById('count');
+                span.innerHTML = counter.toString();
     
-}
-img.onclick=function(){
-
-var interval = setInterval(moveRight,50);
- 
-    
-    
+                
+            }
+        }
+        
+    };
+    //Render the variable in the correct span
+   request.open('GET','http://pramgopi01.imad.hasura-app.io/counter',true); 
+   request.send(null);  
 };
