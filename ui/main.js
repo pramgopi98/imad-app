947e8e763028ebcf766d660936c06afadcb5da81
pramgopi98 <30866528+pramgopi98@users.noreply.github.com>
2017-08-13 14:41:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,8 +21,7 @@ button.onclick=function(){
    request.send(null);  
 };
 //submit name
-var nameInput = document.getElementById('name');
-var name= nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
 //make a request to the server and send the name.
@@ -47,6 +46,9 @@ ul.innerHTML = list;
 
 }
 };
-request.open('GET','http://pramgopi01.imad.hasura-app.io/submit-name?name='+name,true); 
+   
+   var nameInput = document.getElementById('name');
+   var name= nameInput.value;
+   request.open('GET','http://pramgopi01.imad.hasura-app.io/submit-name?name='+name,true); 
    request.send(null);  
 };
