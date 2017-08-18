be580e07d90e465c053f00c87c0b2f33eaf32d47
pramgopi98 <30866528+pramgopi98@users.noreply.github.com>
2017-08-18 22:42:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -193,7 +193,7 @@ app.get('/submit-name/' , function(req, res){// URL:/submit-name?name=xxxx
 
 app.get('/articles/:articleName', function (req, res) {
     //SELECT * FROM article WHERE title = article-one
-    pool.query("SELECT * FROM article WHERE title = '"+req.params.articleName + "'",function(err,result){
+    pool.query("SELECT * FROM article WHERE title = $1 ",[req.params.articleName],function(err,result){
         if(err){
             res.status(500).send(err.toString());
             
