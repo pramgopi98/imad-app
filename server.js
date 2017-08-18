9b5f9f9b6c4439b3686d2b5e286f0fb5de355e2f
pramgopi98 <30866528+pramgopi98@users.noreply.github.com>
2017-08-18 22:23:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -191,9 +191,26 @@ app.get('/submit-name/' , function(req, res){// URL:/submit-name?name=xxxx
 
 
 
-app.get('/:articleName', function (req, res) {
-    var articleName = req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
+app.get('/articles/:articleName', function (req, res) {
+    
+    pool.query("SELECT * FRO article WHERE title = "+req.params.articleName,function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+            
+        }
+        else{
+            if(result.rows.length===0){
+                res.status(404).send('Article Not Found');
+            }
+            else{
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+        
+    });
+    
+  
 });
 
 app.get('/ui/style.css', function (req, res) {
