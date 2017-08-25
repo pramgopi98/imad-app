b3e46aad443edb090031da17872ec2d8af69d444
pramgopi98 <30866528+pramgopi98@users.noreply.github.com>
2017-08-25 12:22:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,28 +1,5 @@
-//counter code
-var button=document.getElementById('counter');
-button.onclick=function(){
-    //Make a request to counter endpoint
-    var request = new XMLHttpRequest();
-    //capture response and store it in a variable
-    request.onreadystatechange = function(){
-        if(request.readyState===XMLHttpRequest.DONE){
-            //Take some action
-            if(request.status===200){
-              var counter=request.responseText;
-              var span = document.getElementById('count');
-              span.innerHTML = counter.toString();
-               
-    
-                
-            }
-        }
-        
-    };
-    //Render the variable in the correct span
-   request.open('GET','http://pramgopi01.imad.hasura-app.io/counter',true); 
-   request.send(null);  
-};
-//submit name
+
+//submit username/password to login.
  
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
@@ -33,25 +10,24 @@ request.onreadystatechange = function(){
         if(request.readyState===XMLHttprequest.DONE){
             //Take some action
             if(request.status===200){
-               var names = request.responseText;
-               names=JSON.parse(names);
-               var list = '';
-               for(var i =0;i<names.length;i++){
-            list+='<li>'+names[i]+'</li>';
-     }
-var ul = document.getElementById('namelist');
-ul.innerHTML = list; 
-               
-    
-                
-            }
+               alert('Logged in successfully!');
+                }
+                else if (request.status===403){
+                    alert('Username/Password is incorrect');
+                   }
+                   else if(request.status===500){
+                       alert('Something went wrong in the server');
+                   }
+                   
 
 
 }
 };
-   var nameInput = document.getElementById('name');
-   var name= nameInput.value;
+   var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+   
   
-   request.open('GET','http://pramgopi01.imad.hasura-app.io/submit-name?name='+name,true); 
-   request.send(null);  
+   request.open('POST','http://pramgopi01.imad.hasura-app.io/login',true); 
+   request.setRequestHeader('Content-Type','application/json');
+   request.send(JSON.stringify({username:username,password:password}));  
 };
